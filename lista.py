eefbcf669aa249a439da9b9530d81df0bbfbd684
Mariana Ayala <mariana.ayalam1121@gmail.com>
2025-08-21 23:28:54 -0600

    Mas funciones de las listas

--- lista.py
+++ lista.py
@@ -14,3 +14,16 @@ print(my_list)
 
 my_list.pop() #eliminar elementos o la lista completa 
 print(my_list)
+
+test = lambda x: x * 2 #es un tipo de funcion 
+# la x es el parametro que utilizara la funcion
+# lo demas es lo que hara 
+print(test(3)) #para mandarla a llamar es com funcion normal 
+
+test = lambda x: x * 2
+print(list(map(test, [2, 3, 5, 8]))) #aqui estamos llamando una nueva funcion map()
+#esta funcion hace que se puedan juntar dos funciones, comp primer parametro debe ir la funcion 
+#a ejecutar y el segundo argumento se pasa como en donde se ejecutara esa funcion 
+#y para imrpimir la funcion map() debe estar dentro del argumento list()
+print(sum(map(test, [2, 3, 5, 8]))) #si le pasamos la funcion sum, entonces como resultao 
+#nos dara la suma de 4,6,10,16 que es 36 
