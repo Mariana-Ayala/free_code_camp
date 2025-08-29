5bca8d3c1772477c40f88db7e30b7899737750c9
Mariana Ayala <mariana.ayalam1121@gmail.com>
2025-08-28 21:25:14 -0600

    Feat proyecto, completo

--- proyecto.py
+++ proyecto.py
@@ -1,24 +1,49 @@
 def arithmetic_arranger(problems, show_answers=False ):
     
+      #formato de impresion
+    first_line = []
+    second_line = []
+    lines = []
+    answer = []
+    
     #deben de ser solo 5 problemas
     if len(problems)>5:
         return'Error: Too many problems'
 
-    #solo se pueden los operandos + y -
     for problem in problems:
-        parts =problem.split()
-        if parts[1] not in ['+','-']:
+        op1, operator, op2 =problem.split()
+        
+        #solo se pueden operandos + y -
+        if operator not in ['+','-']:
              return "Error: Operator must be '+' or '-'."
          
         #solo permite digitos
-        if parts[0].isdigit()==False or parts[2].isdigit()==False:
+        if not (op1.isdigit() and op2.isdigit()):
             return 'Error: Numbers must only contain digits.'
         
         #maximo 4 digitos
-        if len(parts[0])>4 or len(parts[2])>4:
+        if len(op1)>4 or len(op2)>4:
             return 'Error: Numbers cannot be more than four digits.'
-
-    #regresa la lista original
-    return problems
+        
+        width = max(len(op1), len(op2)) + 2
+        #numero de arriba alineado a la derecha
+        first_line.append(op1.rjust(width))
+        #numero de abajo alineado a la derecha
+        second_line.append(operator + op2.rjust(width - 1))
+        #guiones
+        lines.append('-'*width)
+        
+        #si se piden las respuestas
+        if show_answers:
+            if operator == '+':
+                result = str(int(op1)+int(op2))
+            else:
+                result = str(int(op1)-int(op2))
+            answer.append(result.rjust(width))
+        
+    arranged = '    '.join(first_line)+'\n'+'    '.join(second_line)+'\n'+'    '.join(lines)
+    if show_answers:
+        arranged += '\n'+'    '.join(answer)
+    return arranged
 
 print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
